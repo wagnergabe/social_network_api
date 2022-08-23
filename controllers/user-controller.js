fe7f4e22599c538106ddb530f0fcb3e23f16c309
GSvenny <gabrielswagner7@gmail.com>
2022-08-23 18:48:36 -0500

    User get and create api created

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,2 +1,23 @@
 const { User } = require('../models');
 
+const UserController = {
+    // get all users
+    getAllUsers(req, res) {
+        User.find({})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    //Get User by Id
+
+    //Create New User
+    CreateNewUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+    },
+    
+}

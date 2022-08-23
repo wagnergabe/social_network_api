33a00ccd8297a5fe64a8ce15d3aea515265f55ff
GSvenny <gabrielswagner7@gmail.com>
2022-08-23 01:32:04 -0500

    User model start

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        //Unique
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        //Unique?
+        //match validation
+    },
+    thoughts: [],
+    friends: []
+})

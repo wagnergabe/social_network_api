9aa284f8d9ddffbef79e3565cd0e02d9a7fae5ac
GSvenny <gabrielswagner7@gmail.com>
2022-08-24 01:48:03 -0500

    thoughts routes somewhat success, needs fixing

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,5 +1,40 @@
-const { Thought } = require('../models');
+const { Thought, User } = require('../models');
 
-const thoughtController = {}
+const thoughtController = {
+    //get all thoughts
+
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400)
+        });
+    },
+
+    //get thought by ID
+
+    //Create new thought (needs to be attached to user)
+    createThought({ body }, res) {
+        console.log(body)
+        Thought.create(body)
+            .then((dbThoughtData) => {    
+                return User.findOneAndUpdate(
+                    { _id: body.userId },
+                    { $push: { thoughts: dbThoughtData._id } },
+                    { new: true }   
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id' });
+                    return;
+                }
+                console.log(dbUserData)
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+}
 
 module.exports = thoughtController;
--- models/Thought.js
+++ models/Thought.js
@@ -1,32 +1,5 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat')
-//remember to make this file//
-
-const ThoughtSchema = new Schema ({
-    thoughtText: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 280
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (createdAtVal) => dateFormat(createdAtVal)
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    // reaction: 
-    //     [reactionschema]
-},
-{
-    toJSON: {
-        virtuals: true,
-        getters: true
-    }
-});
 
 const ReactionSchema = new Schema({
     reactionId: {
@@ -53,8 +26,35 @@ const ReactionSchema = new Schema({
         getters: true,
         virtuals: true,
     },
+});
+
+
+const ThoughtSchema = new Schema ({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reaction: [ReactionSchema]// think reaction Schema needs to be declared first
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
     id: false
 });
+
 
 
 ThoughtSchema.virtual('reactionCount').get(function() {
